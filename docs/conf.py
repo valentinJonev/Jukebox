afffb4145bfa85a1cecab8f0c91629ca910a1021
Frozen <marto_dimitrov@mail.bg>
2016-06-21 02:38:20 +0300

    Update 1.5

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Belot documentation build configuration file, created by
+# Jukebox documentation build configuration file, created by
 # sphinx-quickstart on Fri May  6 19:39:57 2016.
 #
 # This file is execfile()d with the current directory set to its
@@ -47,7 +47,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Belot'
+project = 'Jukebox'
 copyright = '2016, Valentin Yonev & Martin Dimitrov'
 author = 'Valentin Yonev, Martin Dimitrov'
 
@@ -122,7 +122,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
-#html_title = 'Belot v1.0'
+#html_title = 'Jukebox v1.0'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -204,7 +204,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Belotdoc'
+htmlhelp_basename = 'Jukeboxdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 numpydoc_show_class_members = False
@@ -227,7 +227,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Belot.tex', 'Belot Documentation',
+    (master_doc, 'Jukebox.tex', 'Jukebox Documentation',
      'Valentin Yonev, Martin Dimitrov', 'manual'),
 ]
 
@@ -257,7 +257,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'belot', 'Belot Documentation',
+    (master_doc, 'Jukebox', 'Jukebox Documentation',
      [author], 1)
 ]
 
@@ -271,8 +271,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Belot', 'Belot Documentation',
-     author, 'Belot', 'One line description of project.',
+    (master_doc, 'Jukebox', 'Jukebox Documentation',
+     author, 'Jukebox', 'One line description of project.',
      'Miscellaneous'),
 ]
 
